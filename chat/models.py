c1d15c18e78e83ce53110da9f16884d32a2547e9
SEUNGMIN KIM <smk950226@naver.com>
2019-01-28 16:03:04 +0900

    change consumer from async to async for saving and create message model

---
+++ chat/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.user.username
+
+    def last_10_messages(self):
+        return Message.objects.order_by('-timestamp').all()[:10]
