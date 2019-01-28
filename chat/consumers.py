7c3c3e4780024ce6ff1d4a3ffb559865ce0a0cb9
SEUNGMIN KIM <smk950226@naver.com>
2019-01-28 16:28:35 +0900

    preload messages

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,8 +1,38 @@
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
+from .models import Message
 import json
 
 class ChatConsumer(WebsocketConsumer):
+
+    def fetch_messages(self, data):
+        messages = Message.last_10_messages()
+        content = {
+            'messages': self.messages_to_json(messages)
+        }
+        self.send_message(content)
+
+    def new_message(self, data):
+        print('new')
+    
+    commands = {
+        'fetch_messages': fetch_messages,
+        'new_message': new_message
+    }
+
+    def messages_to_json(self, messages):
+        result = []
+        for message in messages:
+            result.append(self.message_to_json(message))
+        return result
+    
+    def message_to_json(self, message):
+        return {
+            'user': message.user.username,
+            'content': message.content,
+            'timestamp': str(message.timestamp)
+        }
+
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
@@ -24,8 +54,10 @@ class ChatConsumer(WebsocketConsumer):
 
     # Receive message from WebSocket
     def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        data = json.loads(text_data)
+        self.commands[data['command']](self, data)
+    
+    def send_chat_message(self, message):
 
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
@@ -36,11 +68,13 @@ class ChatConsumer(WebsocketConsumer):
             }
         )
 
-    # Receive message from room group
+    # Receive message from room group, 새 메세지
     def chat_message(self, event):
         message = event['message']
 
         # Send message to WebSocket
-        async_to_sync(self.send(text_data=json.dumps({
-            'message': message
-        })))
+        self.send(text_data=json.dumps(message))
+    
+    #preload된 메세지들
+    def send_message(self, message):
+        self.send(text_data=json.dumps(message))
