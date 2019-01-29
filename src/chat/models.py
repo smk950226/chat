fbbfcfc4c4bed458f72bace97c12d7d318fc20c7
SEUNGMIN KIM <smk950226@naver.com>
2019-01-29 21:47:25 +0900

    change model

--- src/chat/models.py
+++ src/chat/models.py
@@ -3,13 +3,28 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class Message(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add = True)
+class Contact(models.Model):
+    user = models.ForeignKey(User, related_name='friends', on_delete=models.CASCADE)
+    friends = models.ManyToManyField('self', blank = True)
 
     def __str__(self):
         return self.user.username
 
-    def last_10_messages():
-        return Message.objects.order_by('-timestamp').all()[:10]
+class Message(models.Model):
+    contact = models.ForeignKey(Contact, related_name='messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.contact.user.username
+
+
+class Chat(models.Model):
+    participants = models.ManyToManyField(Contact, related_name='chats')
+    messages = models.ManyToManyField(Message, blank = True)
+
+    def last_10_messages(self):
+        return self.messages.objects.order_by('-timestamp').all()[:10]
+    
+    def __str__(self):
+        return "{}".format(self.pk)
