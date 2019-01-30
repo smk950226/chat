85409f29df8122a9eab1c38bd5a351d3be9c5129
SEUNGMIN KIM <smk950226@naver.com>
2019-01-31 03:14:41 +0900

    change serializer create method

--- src/chat/api/serializers.py
+++ src/chat/api/serializers.py
@@ -1,7 +1,24 @@
 from rest_framework import serializers
-from chat.models import Chat
+from chat.models import Chat, Contact
+from chat.views import get_user_contact
+
+class ContactSerializer(serializers.StringRelatedField):
+    def to_internal_value(self, value):
+        return value
 
 class ChatSerializer(serializers.ModelSerializer):
+    participants = ContactSerializer(many = True)
+
     class Meta:
         model = Chat
-        fields = ('__all__')
+        fields = ('id', 'messages', 'participants')
+
+    def create(self, validated_data):
+        participants = validated_data.pop('participants')
+        chat = Chat()
+        chat.save()
+        for username in participants:
+            contact = get_user_contact(username)
+            chat.participants.add(contact)
+        chat.save()
+        return chat
